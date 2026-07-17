4d64102a201f32d06630b9e9b06cc3688d085b0c
gim_z66 <gimholg1992@foxmail.com>
2026-07-17 16:38:16 +0800

    fix stupid

--- src/LFW/dat_translator/xml/xml_to_bg_info.ts
+++ src/LFW/dat_translator/xml/xml_to_bg_info.ts
@@ -1,4 +1,4 @@
-import { Defines } from "../..";
+import { bg_info_new, Defines } from "../..";
 import type { IBgInfo } from "../../defines";
 import type { IXMLElement } from "../../ditto";
 
@@ -10,34 +10,22 @@ import type { IXMLElement } from "../../ditto";
  */
 
 export function xml_to_bg_info(el: IXMLElement, fallbackId: string): IBgInfo {
-  const base: IBgInfo = {
-    name: el.get_str("name") ?? fallbackId,
-    shadow: el.get_str("shadow") ?? "",
-    group: el.get_str_arr("group") ?? ["regular"],
-    left: el.get_num("left") ?? 0,
-    right: el.get_num("right") ?? 0,
-    far: el.get_num("far") ?? 0,
-    near: el.get_num("near") ?? 0,
-    height: el.get_num("height") ?? Defines.MODERN_SCREEN_HEIGHT,
-    shadow_w: el.get_num('shadow_w') ?? 0,
-    shadow_h: el.get_num('shadow_h') ?? 0,
-    zoom_x: el.get_num('zoom_x') ?? 0,
-    zoom_y: el.get_num('zoom_y') ?? 0,
-    zoom_z: el.get_num('zoom_z') ?? 0,
-  };
-
+  const base: IBgInfo = bg_info_new();
   const bound = el.get_num_arr("bound");
-  if (bound && bound.length > 0) base.left ??= bound[0];
-  if (bound && bound.length > 1) base.right ??= bound[1];
-  if (bound && bound.length > 2) base.far ??= bound[2];
-  if (bound && bound.length > 3) base.near ??= bound[3];
-
   const zoom = el.get_num_arr("zoom");
-  base.zoom_x ??= zoom?.[0];
-  base.zoom_y ??= zoom?.[1];
-  base.zoom_z ??= zoom?.[2];
   const shadowsize = el.get_num_arr("shadowsize");
-  if (shadowsize && shadowsize.length > 0) base.shadow_w ??= shadowsize[0];
-  if (shadowsize && shadowsize.length > 1) base.shadow_h ??= shadowsize[1];
+  base.name   /**/ = el.get_str("name") ?? fallbackId
+  base.shadow /**/ = el.get_str("shadow") ?? base.shadow
+  base.group  /**/ = el.get_str_arr("group") ?? ["regular"]
+  base.left   /**/ = bound?.[0] ?? el.get_num("left") ?? base.left
+  base.right  /**/ = bound?.[1] ?? el.get_num("right") ?? base.right
+  base.far    /**/ = bound?.[2] ?? el.get_num("far") ?? base.far
+  base.near   /**/ = bound?.[3] ?? el.get_num("near") ?? base.near
+  base.height   /**/ = el.get_num("height") ?? Defines.MODERN_SCREEN_HEIGHT
+  base.shadow_w /**/ = shadowsize?.[0] ?? el.get_num('shadow_w') ?? base.shadow_w
+  base.shadow_h /**/ = shadowsize?.[1] ?? el.get_num('shadow_h') ?? base.shadow_h
+  base.zoom_x   /**/ = zoom?.[0] ?? el.get_num('zoom_x') ?? base.zoom_x
+  base.zoom_y   /**/ = zoom?.[1] ?? el.get_num('zoom_y') ?? base.zoom_y
+  base.zoom_z   /**/ = zoom?.[2] ?? el.get_num('zoom_z') ?? base.zoom_z
   return base;
 }
