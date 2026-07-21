4e8aad6f5b110bda59496d5852f576183681641f
gim_mibook_pro <gimholg1992@foxmail.com>
2026-07-21 22:13:17 +0800

    .

--- src/LFW/WorldDataset.ts
+++ src/LFW/WorldDataset.ts
@@ -112,7 +112,7 @@ export class WorldDataset implements IWorldDataset {
   whirlwind_acc_z: number = 0.5;
   outline_enabled: number = 1;
   entity_flags: number = 0;
-  bg_flags: number = 1;
+  bg_flags: number = 0;
   UPS: number = 60;
   playrate: number = 1;
   atom_time: number = 1;
