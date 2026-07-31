6c7b2d6a52094c8295a7c709675eaf747dd03a57
gim <gimholg1992@foxmail.com>
2026-07-31 21:44:38 +0800

    。

--- src/LFW/bot/state/BotState_StageEnd.ts
+++ src/LFW/bot/state/BotState_StageEnd.ts
@@ -1,8 +1,11 @@
 
-import { AGK, GK, StateEnum } from "../../defines";
+import { AGK, GK, SE, StateEnum } from "../../defines";
 import { BSE } from "../../defines/BotStateEnum";
 import { BotState_Base } from "./BotState";
 
+/**
+ * 关卡结束
+ */
 export class BotState_StageEnd extends BotState_Base {
   override key = BSE.StageEnd;
   override enter(): void {
@@ -13,14 +16,31 @@ export class BotState_StageEnd extends BotState_Base {
   }
   override update(dt: number): BSE | undefined {
     if (this.me.hp <= 0) return BSE.Dead;
-    const c = this.ctrl;
-    const me = c.entity;
+    const { c, me, stage } = this
     this.handle_block();
-    if (!c.world.stage.is_stage_finish) {
+
+    /*
+    章结束，什么都不用干
+    */
+    if (stage.is_chapter_finish)
+      return;
+
+    const stage_end = stage.is_stage_finish;
+    const running = me.state == SE.Running
+
+    /* 
+    节未结束，可能已经进入下一节，故停止奔跑
+    */
+    if (!stage_end && running) {
       c.key_down(me.facing > 0 ? GK.L : GK.R)
       return BSE.Idle;
     }
-    if (me.state !== StateEnum.Running)
+
+    /*
+    目前全都是往右跑进入下一节
+    */
+    if (stage_end && !running) {
       c.key_down(GK.R).key_up(...AGK);
+    }
   }
 }
