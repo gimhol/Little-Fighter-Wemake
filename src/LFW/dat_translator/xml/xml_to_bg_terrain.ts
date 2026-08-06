5a3606d16c0db590c3e7615a16fa4462a4eccfe8
gim <gimholg1992@foxmail.com>
2026-08-06 19:19:57 +0800

    stupid fix

--- src/LFW/dat_translator/xml/xml_to_bg_terrain.ts
+++ src/LFW/dat_translator/xml/xml_to_bg_terrain.ts
@@ -5,14 +5,14 @@ import { reorder_keys } from "../../fields";
 
 export function xml_to_bg_terrain(el: IXMLElement): ITerrainInfo {
   const ret = terrain_info_new();
-  ret.type = el.get_num('type', 0);
+  ret.type = el.get_num('type', ret.type);
   ret.name = el.get_str('name');
-  ret.x1 = el.get_num('x1', 0);
-  ret.x2 = el.get_num('x2', 0);
-  ret.z1 = el.get_num('z1', 0);
-  ret.z2 = el.get_num('z2', 0);
-  ret.h1 = el.get_num('h1', 0);
-  ret.h2 = el.get_num('h2', 0);
+  ret.x1 = el.get_num('x1', ret.x1);
+  ret.x2 = el.get_num('x2', ret.x2);
+  ret.z1 = el.get_num('z1', ret.z1);
+  ret.z2 = el.get_num('z2', ret.z2);
+  ret.h1 = el.get_num('h1', ret.h1);
+  ret.h2 = el.get_num('h2', ret.h2);
   reorder_keys(ret, terrain_info_fields);
   return ret;
 }
