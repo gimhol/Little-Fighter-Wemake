82c00c4caa1a07f9b6cd678aa8d918a229a57f0f
gim <gimholg1992@foxmail.com>
2026-09-05 21:22:12 +0800

    f9

--- src/LFW/cmds/CMD_F9.ts
+++ src/LFW/cmds/CMD_F9.ts
@@ -11,5 +11,5 @@ CMDS.register(CMD.F9, help, (c) => {
   if (c.world.fn_locked) return Ditto.debug(`F9 failed, Fn Locked.`)
   if (c.world.stage_limit) return Ditto.debug(`F9 failed, Stage Limited.`)
   c.world.add_count(CMD.F9, 1)
-  for (const e of c.world.entities) if (is_weapon(e)) e.hp = 0;
+  for (const e of Array.from(c.world.entities)) if (is_weapon(e)) e.hp = 0;
 })
